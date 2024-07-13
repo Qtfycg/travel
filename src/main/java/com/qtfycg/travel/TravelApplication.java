1eceafdc8c29c78abf985ec4e065da0a5d20d52f
“qtfycg” <1264318507@qq.com>
2024-07-13 22:51:48 +0800

    7.13

--- src/main/java/com/qtfycg/travel/TravelApplication.java
+++ src/main/java/com/qtfycg/travel/TravelApplication.java
@@ -1,13 +1,16 @@
 package com.qtfycg.travel;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class TravelApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TravelApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TravelApplication.class, args);
+        log.info("-----------项目后端启动成功----------");
+    }
 
 }
